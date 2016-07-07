4772cad7ac13c592bd5b15b2275735f3b1d9e0fc
Jorge Lopez <jorginho@riseup.net>
2016-07-07 19:29:45 +0200

    Probas con hightchart dentro do dashbord

--- web/src/components/Dashboard/index.js
+++ web/src/components/Dashboard/index.js
@@ -5,12 +5,12 @@ import CSSModules from 'react-css-modules';
 import styles from './styles.scss';
 
 import 'style!css!react-grid-layout/css/styles.css'
-import 'style!css!./style.css'
 import RaisedButton from 'material-ui/RaisedButton';
 
 var WidthProvider = require('react-grid-layout').WidthProvider;
 var ReactGridLayout = require('react-grid-layout');
 ReactGridLayout = WidthProvider(ReactGridLayout);
+const ReactHighcharts = require('react-highcharts');
 
 @CSSModules(styles)
 class Dashboard extends Component {
@@ -25,13 +25,38 @@ class Dashboard extends Component {
     render() {
 		const {dashboard} = this.props;
 
+        var config = {
+            chart: {
+                type: 'pie'
+            },
+            title: {
+                text: 'Title'
+            },
+
+            series: [{
+                data: [{
+                    name: 'Name 1',
+                    y: 56.33
+                }, {
+                    name: 'Name 2',
+                    y: 24.03,
+                }, {
+                    name: 'Name 3',
+                    y: 10.38
+                }, {
+                    name: 'Name 4',
+                    y: 4.77
+                }]
+            }]
+        }
+
     	return (
             <div>
         		<ReactGridLayout onLayoutChange={this.changeLayout} items={3} rowHeight={30} cols={12} autoSize={true} className="layout">
                     {dashboard.layout.map((l, index) => {
                         return (
                             <div key={index} _grid={l} className={styles.cell}>
-                                <img className={styles.container} src="http://concepto.de/wp-content/uploads/2015/03/Paisaje.jpg"/>
+                                <ReactHighcharts className={styles.container} config={config} ref="chart"></ReactHighcharts>
                             </div>
                         )
                     })}
